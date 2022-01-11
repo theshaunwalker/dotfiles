acfbd7fd483bbcaa85b415622d57e5e25674d194
Shaun Walker <shaunandrewwalker@gmail.com>
2022-01-11 17:56:01 +0000

    Add PHP topic with composer install and 8.1 brew package

---
+++ php/install.sh
@@ -0,0 +1,26 @@
+install_composer() {
+    EXPECTED_CHECKSUM="$(php -r 'copy("https://composer.github.io/installer.sig", "php://stdout");')"
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+    if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+    then
+        >&2 echo 'ERROR: Invalid installer checksum'
+        rm composer-setup.php
+        exit 1
+    fi
+
+    php composer-setup.php --quiet
+    rm composer-setup.php
+    mv composer.phar ~/bin/composer
+}
+
+
+# Check for Homebrew
+if test ! $(which composer)
+    composer self-update
+then
+    install_composer
+fi
+
+exit 0
